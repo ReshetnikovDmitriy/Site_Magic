7ed1d573bb372db2d63da782ddf7a96bb7d1ed76
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2025-08-06 11:33:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Menu
+function openModal() {
+    document.getElementById("Notification").style.top = "0px";
+}
+function closeModal() {
+    document.getElementById("Notification").style.top = "-400px";
+}
